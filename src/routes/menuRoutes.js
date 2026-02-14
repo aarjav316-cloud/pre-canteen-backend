fc1ba29791e891d7d68595420b010d89f34db321
Aarjav jain <aarjav316@gmail.com>
2026-02-14 13:04:35 +0530

    menu route added

--- src/routes/menuRoutes.js
+++ src/routes/menuRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { 
     addMenuItems, 
+    deleteMenu, 
     getAllMenu,
-    searchMenu
+    searchMenu,
+    toggleAvailability,
+    updatedMenu
 } from '../controllers/menuController.js'
 
 import { authorizeRoles , protect } from '../middleware/authMiddleware.js'
@@ -14,6 +17,11 @@ router.get("/" , getAllMenu)
 router.get("/search" , searchMenu)
 
 router.post("/" , protect , authorizeRoles("admin") , addMenuItems)
+
+router.put("/:id" , protect , authorizeRoles("admin") , updatedMenu)
+router.delete("/:id" , protect , authorizeRoles("admin") , deleteMenu)
+router.patch("/:id/toggle" , protect , authorizeRoles("admin") , toggleAvailability)
+
 
 
 export default router
